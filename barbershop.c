7331c18d877941d43340337056b36432b991e784
Xin Li <urumican@users.noreply.github.com>
2014-11-09 10:41:05 -0800

    Update barbershop.c

--- barbershop.c
+++ barbershop.c
@@ -7,21 +7,55 @@
 
 #define NUM_CHAIR 5
 
-typedef enum{SLEEP, WORK, FREE} state_barber;
-typedef enmu{WAIT, CUT} state_customer;
+typedef enum{SLEEP, WORK, FREE} barber_st;
+typedef enmu{WAIT, CUT} customer_st;
 
 pthreat_mutex_t wake_barber;
 pthreat_mutex_t customer_wait;
+pthreat_cond_t condition; // I learned to use conditional variable.
 
 int chair_num = NUM_CHAIR;
+barber_st state_bar = FREE;
+
 
 /* We don't need an additional variable for the barber chair. */
 
+void check_barber(int ID)
+{
+	if(state_bar == FREE)
+	{
+		pthreat_mutex_unlock(customer_wait);
+		printf("[Customer %d] Get barber serving.\n\n", ID);
+	}
+	else if(state_bar == WORK)
+	{
+		chair_num--;
+		printf("[Custermer %d] Start wating.\n\n", ID);
+	}
+	else if(state_bar == SLEEP)
+	{
+		pthreat_cond_signal(condition);
+		printf("[Custermer %d] Wake up the sleeping barber.\n\n", ID);
+	}
+}
+
+void cut_hair(int ID)
+
+void get_haircut()
+
 void* barber()
 
-void* custromer()
+void* custromer(void* id)
 {
-	if()
+	int* ID = (int*)id;
+	
+	if(chair_num != 0)
+	{
+		check_barber();
+		
+	}
+	else
+		print("[Customer %d] The shop is full. I am leaving.", *ID);
 }
 
 int main(int argc, char *argv[])
