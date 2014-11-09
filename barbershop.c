5ae2a4b21fbe5fa26b3ddb91cdc1227c4df433ae
Xin Li <urumican@users.noreply.github.com>
2014-11-09 14:36:48 -0800

    Update barbershop.c

--- barbershop.c
+++ barbershop.c
@@ -12,7 +12,7 @@ typedef enum{CHECK, WAIT, READY} customer_st;
 typedef enum{ON, OFF} chair_st;
 
 pthreat_mutex_t wake_barber;
-pthreat_mutex_t customer_wait;
+pthreat_mutex_t mutex_cust;
 pthreat_cond_t condition; // I learned to use conditional variable.
 
 int chair_num = NUM_CHAIR;
@@ -23,30 +23,33 @@ chair_st state_ch[NUM_CHAIR];
 
 /* We don't need an additional variable for the barber chair. */
 
-void check_barber(int ID)
+void check_barber()
 {
+	/*
 	if(state_bar == FREE)
 	{
-		pthreat_mutex_unlock(customer_wait);
+		pthreat_mutex_unlock(mutex_cust);
 		printf("[Customer %d] Get barber serving.\n\n", ID);
 	}
 	else if(state_bar == WORK)
 	{
-		chair_num--;
+		//chair_num--;
 		printf("[Custermer %d] Start wating.\n\n", ID);
 	}
-	else if(state_bar == SLEEP)
+	*/
+	if(state_bar == SLEEP)
 	{
+		/*
 		pthread_mutex_lock(&wake_barber);
 		chair_num--; // Sit in the waiting room first
-		
+		*/
 		/* Then try to wake barker*/
 		if(chair < 5)
 		{
 			pthread_cond_signal(&condition);
 		}
-		pthread_mutex_unlock(&wake_barber);
-		pthread_mutex_unlock(&customer_wait);
+	//	pthread_mutex_unlock(&wake_barber);
+		pthread_mutex_unlock(&mutex_cust);
 		printf("[Custermer %d] Wake up the sleeping barber.\n\n", ID);
 	}
 }
@@ -72,11 +75,18 @@ void* barber()
 			state_bar = SLEEP;
 			pthread_cond_wait(&condition, &wake_barber);
 		}
-		
 		chair_num++;
+		pthread_mutex_unlock(&wake_barber);
+		
 		state_bar = WORK;
 		cut();
 		
+		state_bar = FREE;
+		//pthread_unlock_unlock(&mutex_cust);
+		if(chair_nu < 5) // It mean there is someone still waiting.
+		{ 
+			pthread_unlock_unlock(&mutex_cust);
+		}
 	}
 }
 
@@ -86,15 +96,19 @@ void* custromer(void* id)
 	
 	if(chair_num != 0)
 	{
+		pthread_mutex_lock(&wake_barber);
+		chair_num--; // Sit in the waiting room first
 		check_barber();
-		pthread_mutex_lock(customer_wait);
+		pthread_mutex_unlock(&wake_barber);
+		
+		pthread_mutex_lock(&mutex_cust);
 		
 		get_cut(); // Being cut.
 		
 		
 	}
 	else
-		print("[Customer %d] The shop is full. I am leaving.", *ID);
+		printf("[Customer %d] The shop is full. I am leaving.", *ID);
 }
 
 int main(int argc, char *argv[])
